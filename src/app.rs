ac262938601416600826dc67696c854797cf0d79
Gabriel Lee <gelos@unknown845cf33cbfb3.attlocal.net>
2022-05-13 09:54:23 -0400

    Added modes

--- src/app.rs
+++ src/app.rs
@@ -1,26 +1,35 @@
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
-pub struct TemplateApp {
-    // Example stuff:
+pub struct ClassNotes {
     label: String,
 
     // this how you opt-out of serialization of a member
     #[serde(skip)]
     value: f32,
+
+    mode: Mode,
 }
 
-impl Default for TemplateApp {
+#[derive(serde::Deserialize, serde::Serialize)]
+#[derive(PartialEq)]
+enum Mode {
+    NONE,
+    NOTES,
+}
+
+impl Default for ClassNotes {
     fn default() -> Self {
         Self {
             // Example stuff:
             label: "Hello World!".to_owned(),
             value: 3.1,
+            mode: Mode::NOTES,
         }
     }
 }
 
-impl TemplateApp {
+impl ClassNotes {
     /// Called once before the first frame.
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
         // This is also where you can customized the look at feel of egui using
@@ -34,36 +43,28 @@ impl TemplateApp {
 
         Default::default()
     }
-}
 
-impl eframe::App for TemplateApp {
-    /// Called by the frame work to save state before shutdown.
-    fn save(&mut self, storage: &mut dyn eframe::Storage) {
-        eframe::set_value(storage, eframe::APP_KEY, self);
+    pub fn update_none(
+        &mut self, ctx: &egui::Context, _frame: &mut eframe::Frame
+    ) {
+        egui::CentralPanel::default().show(ctx, |_| {});
     }
 
-    /// Called each time the UI needs repainting, which may be many times per second.
-    /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
-    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-        let Self { label, value } = self;
-
-        // Examples of how to create different panels and windows.
-        // Pick whichever suits you.
-        // Tip: a good default choice is to just keep the `CentralPanel`.
-        // For inspiration and more examples, go to https://emilk.github.io/egui
-
-        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-            // The top panel is often a good place for a menu bar:
-            egui::menu::bar(ui, |ui| {
-                ui.menu_button("File", |ui| {
-                    if ui.button("Quit").clicked() {
-                        frame.quit();
-                    }
-                });
-            });
-        });
+    pub fn update_notes(
+        &mut self, ctx: &egui::Context, _frame: &mut eframe::Frame
+    ) {
+        let Self { label, value, .. } = self;
 
         egui::SidePanel::left("side_panel").show(ctx, |ui| {
+            egui::ScrollArea::vertical().show(ui, |ui| {
+                egui::CollapsingHeader::new("heading").show(ui, |ui| {
+                    ui.label("Contents");
+                });
+                egui::CollapsingHeader::new("heading2").show(ui, |ui| {
+                    ui.label("Contents");
+                });
+            });
+
             ui.heading("Side Panel");
 
             ui.horizontal(|ui| {
@@ -107,5 +108,47 @@ impl eframe::App for TemplateApp {
                 ui.label("You would normally chose either panels OR windows.");
             });
         }
+
+    }
+}
+
+impl eframe::App for ClassNotes {
+    /// Called by the frame work to save state before shutdown.
+    fn save(&mut self, storage: &mut dyn eframe::Storage) {
+        eframe::set_value(storage, eframe::APP_KEY, self);
+    }
+
+    /// Called each time the UI needs repainting, which may be many times per second.
+    /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        // Examples of how to create different panels and windows.
+        // Pick whichever suits you.
+        // Tip: a good default choice is to just keep the `CentralPanel`.
+        // For inspiration and more examples, go to https://emilk.github.io/egui
+
+        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
+            // The top panel is often a good place for a menu bar:
+            egui::menu::bar(ui, |ui| {
+                ui.menu_button("Mode", |ui| {
+                    let mut selected: Option<Mode> = None;
+                    if ui.button("Notes").clicked() {
+                        selected = Some(Mode::NOTES);
+                    } else if ui.button("None").clicked() {
+                        selected = Some(Mode::NONE);
+                    }
+                    if let Some(mode) = selected {
+                        if mode != self.mode {
+                            self.mode = mode;
+                            ctx.request_repaint();
+                        }
+                    }
+                });
+            });
+        });
+
+        match self.mode {
+            Mode::NONE => self.update_none(ctx, frame),
+            Mode::NOTES => self.update_notes(ctx, frame),
+        };
     }
 }
